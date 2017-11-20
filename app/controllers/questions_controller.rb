e46330d10b087b78f3ece6ef4b4abf8a2a876dd5
gabrielibanez <g.ibanezw@gmail.com>
2017-11-20 14:12:13 +0100

    coach est chaud

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -6,5 +6,14 @@ class QuestionsController < ApplicationController
   end
 
   def answer
+    @answer = params[:questions]
+
+    if @answer == "I am going to work right now"
+      @answer = "Great!"
+    elsif @answer.include? "?"
+      @answer = "Silly question, get dressed and go to work!"
+    else
+      @answer = "I don't care, get dressed and go to work!"
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
     #Action
-    root to: 'questions#home'
     get 'ask', to: 'questions#ask', as: :ask
     get 'answer', to: 'questions#answer', as: :answer
   end
